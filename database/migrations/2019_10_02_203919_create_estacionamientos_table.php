9a5de12c2ac84af8e01fabf4040094228334ac64
ajruizr <ajruizr@hotmail.com>
2019-10-06 20:48:41 -0500

    Migracion Estacionamiento y Plantilla Bootsrap

---
+++ database/migrations/2019_10_02_203919_create_estacionamientos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstacionamientosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estacionamientos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('lugar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estacionamientos');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/index', 'EstacionamientoController@index')->name('home');
+
+Route::resource('estacionamiento','EstacionamientoController'); //con este nos evitamos todo el mapeo
